7bff5a2b9de38b6a9585214f1206970c9d5767ec
millerer <eric.miller.nw@gmail.com>
2016-10-01 22:40:36 -0700

    Added gpl software license v3 disclaimer to code files

--- www/js/pd_poleax.js
+++ www/js/pd_poleax.js
@@ -1,4 +1,19 @@
- //The below functions are for printing out chapters of pictures and text to the screen after user selection
+ /*	Pocket Armizare: A HEMA reference mobile application for the knightly martial art of Armizare 
+	 
+    Copyright (C) 2016  by Eric Miller
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>  */
 
 /*
 	jqeuery syntax to retain css for ajax appended elements from:
